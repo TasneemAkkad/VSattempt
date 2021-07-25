41f9d492217918c1ab3fb7e4073166ee8cfc5383
TasneemAkkad <tasneem.akkad.1995@putlook.com>
2021-07-25 17:34:35 +0300

    Initial check-in of module MyDictionary

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> dictionary = new MyDictionary<int, string>();
+            dictionary.Add(1, "Tasneem");
+            dictionary.Add(2, "Celine");
+            dictionary.Add(3, "Bana");
+            dictionary.Add(4, "Yser");
+
+            
+
+            Console.WriteLine("1");
+
+        }
+        
+    }
+}
